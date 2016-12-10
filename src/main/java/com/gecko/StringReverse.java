80bee63cf8fb71b6d28e26c70a7763e6b5c66677
ha-li <ha-li@gmail.com>
2016-12-10 11:32:38 -0800

    remove .iml file, string reverse in java is impossible due to final class

---
+++ src/main/java/com/gecko/StringReverse.java
@@ -0,0 +1,20 @@
+package com.gecko;
+
+/**
+ * Created by hlieu on 12/10/16.
+ */
+public class StringReverse {
+
+   public String reverse (String s) {
+      int length = s.length ();
+
+      // this is impossible to do in java because strings are final class
+      // so you cannot replace a char in the string, you would have
+      // to create a new string, so its not very convenient
+      for (int i = 0; i < length/2; i++ ){
+
+      }
+
+      return "impossible in java";
+   }
+}
